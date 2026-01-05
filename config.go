ba7277718ebaf546c35d0421f2e7f5ec4ae551fb
Michiel De Backker <mail@backkem.me>
2026-01-05 22:10:09 +0100

    Functional options pattern with NewServer() API

--- config.go
+++ config.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 package mdns
@@ -8,17 +8,18 @@ import (
 	"time"
 
 	"github.com/pion/logging"
+	"golang.org/x/net/dns/dnsmessage"
 )
 
 const (
 	// DefaultAddressIPv4 is the default used by mDNS
 	// and in most cases should be the address that the
-	// ipv4.PacketConn passed to Server is bound to.
+	// ipv4.PacketConn passed to Server or NewServer is bound to.
 	DefaultAddressIPv4 = "224.0.0.0:5353"
 
 	// DefaultAddressIPv6 is the default IPv6 address used
 	// by mDNS and in most cases should be the address that
-	// the ipv6.PacketConn passed to Server is bound to.
+	// the ipv6.PacketConn passed to Server or NewServer is bound to.
 	DefaultAddressIPv6 = "[FF02::]:5353"
 )
 
@@ -28,22 +29,133 @@ type Config struct {
 	Name string
 
 	// QueryInterval controls how often we sends Queries until we
-	// get a response for the requested name
+	// get a response for the requested name.
 	QueryInterval time.Duration
 
 	// LocalNames are the names that we will generate answers for
-	// when we get questions
+	// when we get questions.
 	LocalNames []string
 
 	// LocalAddress will override the published address with the given IP
 	// when set. Otherwise, the automatically determined address will be used.
 	LocalAddress net.IP
 
+	// LoggerFactory is used to create a logger for the server.
 	LoggerFactory logging.LoggerFactory
 
-	// IncludeLoopback will include loopback interfaces to be eligble for queries and answers.
+	// IncludeLoopback will include loopback interfaces to be eligible for queries and answers.
 	IncludeLoopback bool
 
 	// Interfaces will override the interfaces used for queries and answers.
 	Interfaces []net.Interface
+}
+
+// ServerOption configures a Server.
+type ServerOption interface {
+	applyServer(*ServerConfig)
+}
+
+// ClientOption configures a Client.
+type ClientOption interface {
+	applyClient(*ClientConfig)
+}
+
+// ClientConfig holds configuration for a future dedicated Client type.
+// Currently used to demonstrate shared options between Server and Client.
+type ClientConfig struct {
+	Name            string
+	QueryInterval   time.Duration
+	LoggerFactory   logging.LoggerFactory
+	IncludeLoopback bool
+	Interfaces      []net.Interface
+}
+
+// nameOption sets the name for logging.
+type nameOption string
+
+// WithName sets the name used for logging purposes.
+func WithName(name string) nameOption {
+	return nameOption(name)
+}
+
+func (o nameOption) applyServer(c *ServerConfig) { c.Name = string(o) }
+func (o nameOption) applyClient(c *ClientConfig) { c.Name = string(o) }
+
+// loggerFactoryOption sets the logger factory.
+type loggerFactoryOption struct {
+	factory logging.LoggerFactory
+}
+
+// WithLoggerFactory sets the logger factory for creating loggers.
+func WithLoggerFactory(factory logging.LoggerFactory) loggerFactoryOption {
+	return loggerFactoryOption{factory: factory}
+}
+
+func (o loggerFactoryOption) applyServer(c *ServerConfig) { c.LoggerFactory = o.factory }
+func (o loggerFactoryOption) applyClient(c *ClientConfig) { c.LoggerFactory = o.factory }
+
+// includeLoopbackOption sets whether to include loopback interfaces.
+type includeLoopbackOption bool
+
+// WithIncludeLoopback sets whether loopback interfaces should be included.
+func WithIncludeLoopback(include bool) includeLoopbackOption {
+	return includeLoopbackOption(include)
+}
+
+func (o includeLoopbackOption) applyServer(c *ServerConfig) { c.IncludeLoopback = bool(o) }
+func (o includeLoopbackOption) applyClient(c *ClientConfig) { c.IncludeLoopback = bool(o) }
+
+// interfacesOption sets the interfaces to use.
+type interfacesOption []net.Interface
+
+// WithInterfaces sets the network interfaces to use.
+// If not set, all suitable interfaces will be discovered automatically.
+func WithInterfaces(ifaces ...net.Interface) interfacesOption {
+	return interfacesOption(ifaces)
+}
+
+func (o interfacesOption) applyServer(c *ServerConfig) { c.Interfaces = []net.Interface(o) }
+func (o interfacesOption) applyClient(c *ClientConfig) { c.Interfaces = []net.Interface(o) }
+
+// Server-only options
+
+// localNamesOption sets the local names to respond to.
+type localNamesOption []string
+
+// WithLocalNames sets the names that the server will respond to.
+// These are the mDNS names that this server will generate answers for.
+func WithLocalNames(names ...string) localNamesOption {
+	return localNamesOption(names)
+}
+
+func (o localNamesOption) applyServer(c *ServerConfig) { c.LocalNames = []string(o) }
+
+// localAddressOption sets the local address to publish.
+type localAddressOption struct {
+	addr net.IP
+}
+
+// WithLocalAddress sets the IP address to publish in responses.
+// If not set, the address will be automatically determined from the interface.
+func WithLocalAddress(addr net.IP) localAddressOption {
+	return localAddressOption{addr: addr}
+}
+
+func (o localAddressOption) applyServer(c *ServerConfig) { c.LocalAddress = o.addr }
+
+// recordTypesOption limits which record types are processed.
+type recordTypesOption []dnsmessage.Type
+
+// WithRecordTypes limits which DNS record types the server will process.
+// By default (if not called), all record types are allowed - no filtering.
+//
+// For WebRTC/ICE usage (legacy behavior), restrict to address records:
+//
+//	mdns.WithRecordTypes(dnsmessage.TypeA, dnsmessage.TypeAAAA)
+func WithRecordTypes(types ...dnsmessage.Type) recordTypesOption {
+	return recordTypesOption(types)
+}
+
+func (o recordTypesOption) applyServer(c *ServerConfig) {
+	c.AllowedRecordTypes = []dnsmessage.Type(o)
 }
---
+++ server_test.go
@@ -0,0 +1,423 @@
+// SPDX-FileCopyrightText: The Pion community <https://pion.ly>
+// SPDX-License-Identifier: MIT
+
+//go:build !js
+
+package mdns
+
+import (
+	"net"
+	"testing"
+
+	"github.com/pion/logging"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/dns/dnsmessage"
+	"golang.org/x/net/ipv4"
+	"golang.org/x/net/ipv6"
+)
+
+func TestWithName(t *testing.T) {
+	cfg := &ServerConfig{}
+	opt := WithName("test-server")
+	opt.applyServer(cfg)
+
+	assert.Equal(t, "test-server", cfg.Name)
+}
+
+func TestWithLocalNames(t *testing.T) {
+	cfg := &ServerConfig{}
+	opt := WithLocalNames("foo.local", "bar.local")
+	opt.applyServer(cfg)
+
+	assert.Equal(t, []string{"foo.local", "bar.local"}, cfg.LocalNames)
+}
+
+func TestWithLocalAddress(t *testing.T) {
+	cfg := &ServerConfig{}
+	ip := net.ParseIP("192.168.1.100")
+	opt := WithLocalAddress(ip)
+	opt.applyServer(cfg)
+
+	assert.Equal(t, ip, cfg.LocalAddress)
+}
+
+func TestWithLoggerFactory(t *testing.T) {
+	cfg := &ServerConfig{}
+	factory := logging.NewDefaultLoggerFactory()
+	opt := WithLoggerFactory(factory)
+	opt.applyServer(cfg)
+
+	assert.Equal(t, factory, cfg.LoggerFactory)
+}
+
+func TestWithIncludeLoopback(t *testing.T) {
+	cfg := &ServerConfig{}
+
+	// Test setting to true
+	opt := WithIncludeLoopback(true)
+	opt.applyServer(cfg)
+	assert.True(t, cfg.IncludeLoopback)
+
+	// Test setting to false
+	opt = WithIncludeLoopback(false)
+	opt.applyServer(cfg)
+	assert.False(t, cfg.IncludeLoopback)
+}
+
+func TestWithInterfaces(t *testing.T) {
+	cfg := &ServerConfig{}
+	ifaces := []net.Interface{
+		{Index: 1, Name: "eth0"},
+		{Index: 2, Name: "eth1"},
+	}
+	opt := WithInterfaces(ifaces...)
+	opt.applyServer(cfg)
+
+	assert.Equal(t, ifaces, cfg.Interfaces)
+}
+
+func TestWithRecordTypes(t *testing.T) {
+	cfg := &ServerConfig{}
+
+	// Test setting specific record types (legacy WebRTC/ICE behavior)
+	opt := WithRecordTypes(dnsmessage.TypeA, dnsmessage.TypeAAAA)
+	opt.applyServer(cfg)
+	assert.Equal(t, []dnsmessage.Type{dnsmessage.TypeA, dnsmessage.TypeAAAA}, cfg.AllowedRecordTypes)
+
+	// Test empty (future DNS-SD behavior - all types allowed)
+	cfg2 := &ServerConfig{}
+	assert.Empty(t, cfg2.AllowedRecordTypes)
+}
+
+func TestSharedOptionsWorkWithClientConfig(t *testing.T) {
+	// Verify shared options implement both interfaces
+	clientCfg := &ClientConfig{}
+
+	WithName("client").applyClient(clientCfg)
+	assert.Equal(t, "client", clientCfg.Name)
+
+	factory := logging.NewDefaultLoggerFactory()
+	WithLoggerFactory(factory).applyClient(clientCfg)
+	assert.Equal(t, factory, clientCfg.LoggerFactory)
+
+	WithIncludeLoopback(true).applyClient(clientCfg)
+	assert.True(t, clientCfg.IncludeLoopback)
+
+	ifaces := []net.Interface{{Index: 1, Name: "eth0"}}
+	WithInterfaces(ifaces...).applyClient(clientCfg)
+	assert.Equal(t, ifaces, clientCfg.Interfaces)
+}
+
+func TestMultipleOptionsApplied(t *testing.T) {
+	cfg := &ServerConfig{}
+	factory := logging.NewDefaultLoggerFactory()
+	ip := net.ParseIP("10.0.0.1")
+	ifaces := []net.Interface{{Index: 1, Name: "eth0"}}
+
+	// Apply multiple options
+	opts := []ServerOption{
+		WithName("multi-test"),
+		WithLoggerFactory(factory),
+		WithLocalNames("service.local"),
+		WithLocalAddress(ip),
+		WithIncludeLoopback(true),
+		WithInterfaces(ifaces...),
+		WithRecordTypes(dnsmessage.TypeA),
+	}
+
+	for _, opt := range opts {
+		opt.applyServer(cfg)
+	}
+
+	assert.Equal(t, "multi-test", cfg.Name)
+	assert.Equal(t, factory, cfg.LoggerFactory)
+	assert.Equal(t, []string{"service.local"}, cfg.LocalNames)
+	assert.Equal(t, ip, cfg.LocalAddress)
+	assert.True(t, cfg.IncludeLoopback)
+	assert.Equal(t, ifaces, cfg.Interfaces)
+	assert.Equal(t, []dnsmessage.Type{dnsmessage.TypeA}, cfg.AllowedRecordTypes)
+}
+
+func TestServerConfigDefaults(t *testing.T) {
+	cfg := &ServerConfig{}
+
+	// Verify zero-value defaults
+	assert.Empty(t, cfg.Name)
+	assert.Empty(t, cfg.LocalNames)
+	assert.Nil(t, cfg.LocalAddress)
+	assert.Nil(t, cfg.LoggerFactory)
+	assert.False(t, cfg.IncludeLoopback)
+	assert.Nil(t, cfg.Interfaces)
+	assert.Empty(t, cfg.AllowedRecordTypes)
+}
+
+func TestClientConfigDefaults(t *testing.T) {
+	cfg := &ClientConfig{}
+
+	// Verify zero-value defaults
+	assert.Empty(t, cfg.Name)
+	assert.Zero(t, cfg.QueryInterval)
+	assert.Nil(t, cfg.LoggerFactory)
+	assert.False(t, cfg.IncludeLoopback)
+	assert.Nil(t, cfg.Interfaces)
+}
+
+func TestWithLocalAddressIPv6(t *testing.T) {
+	cfg := &ServerConfig{}
+	ip := net.ParseIP("::1")
+	opt := WithLocalAddress(ip)
+	opt.applyServer(cfg)
+
+	assert.Equal(t, ip, cfg.LocalAddress)
+	assert.True(t, cfg.LocalAddress.To4() == nil, "expected IPv6 address")
+}
+
+func TestWithLocalNamesEmpty(t *testing.T) {
+	cfg := &ServerConfig{}
+	opt := WithLocalNames()
+	opt.applyServer(cfg)
+
+	assert.Empty(t, cfg.LocalNames)
+}
+
+func TestWithInterfacesEmpty(t *testing.T) {
+	cfg := &ServerConfig{}
+	opt := WithInterfaces()
+	opt.applyServer(cfg)
+
+	assert.Empty(t, cfg.Interfaces)
+}
+
+func TestWithRecordTypesEmpty(t *testing.T) {
+	cfg := &ServerConfig{}
+	opt := WithRecordTypes()
+	opt.applyServer(cfg)
+
+	assert.Empty(t, cfg.AllowedRecordTypes)
+}
+
+func TestOptionsImplementInterfaces(t *testing.T) {
+	// Verify compile-time interface compliance by assigning to interface variables.
+	// This test ensures the option types correctly implement their interfaces.
+	var serverOpt ServerOption
+	var clientOpt ClientOption
+
+	// Shared options implement both interfaces
+	serverOpt = WithName("test")
+	clientOpt = WithName("test")
+	_ = serverOpt
+	_ = clientOpt
+
+	serverOpt = WithLoggerFactory(logging.NewDefaultLoggerFactory())
+	clientOpt = WithLoggerFactory(logging.NewDefaultLoggerFactory())
+	_ = serverOpt
+	_ = clientOpt
+
+	serverOpt = WithIncludeLoopback(false)
+	clientOpt = WithIncludeLoopback(false)
+	_ = serverOpt
+	_ = clientOpt
+
+	ifaces := []net.Interface{{Index: 1, Name: "eth0"}}
+	serverOpt = WithInterfaces(ifaces...)
+	clientOpt = WithInterfaces(ifaces...)
+	_ = serverOpt
+	_ = clientOpt
+
+	// Server-only options
+	serverOpt = WithLocalNames("test.local")
+	_ = serverOpt
+
+	serverOpt = WithLocalAddress(net.ParseIP("127.0.0.1"))
+	_ = serverOpt
+
+	serverOpt = WithRecordTypes(dnsmessage.TypeA)
+	_ = serverOpt
+
+	// If this test compiles and runs, the interfaces are satisfied
+}
+
+func TestNewServerErrorNoUsableInterfaces(t *testing.T) {
+	// Interface with no addresses and down flag - should be skipped
+	ifaces := []net.Interface{
+		{Index: 1, Name: "dummy0", Flags: 0}, // not up
+	}
+
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	_, err := NewServer(ipv4.NewPacketConn(sock), nil,
+		WithInterfaces(ifaces...),
+	)
+	assert.Error(t, err)
+}
+
+func TestNewServerErrorNoPacketConn(t *testing.T) {
+	_, err := NewServer(nil, nil)
+	assert.Error(t, err)
+}
+
+func TestNewServerWithDownInterface(t *testing.T) {
+	// Interface that is down should be skipped
+	ifaces := []net.Interface{
+		{Index: 1, Name: "down0", Flags: 0, MTU: 1500}, // FlagUp not set
+	}
+
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	_, err := NewServer(ipv4.NewPacketConn(sock), nil,
+		WithInterfaces(ifaces...),
+	)
+	// Should fail because no usable interfaces
+	assert.Error(t, err)
+}
+
+func TestNewServerWithLoopbackExcluded(t *testing.T) {
+	// When IncludeLoopback is false (default), loopback interfaces should be skipped
+	ifaces := []net.Interface{
+		{Index: 1, Name: "lo", Flags: net.FlagUp | net.FlagLoopback, MTU: 65536},
+	}
+
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	_, err := NewServer(ipv4.NewPacketConn(sock), nil,
+		WithInterfaces(ifaces...),
+		WithIncludeLoopback(false),
+	)
+	// Should fail because loopback is the only interface and it's excluded
+	assert.Error(t, err)
+}
+
+func TestNewServerWithZeroMTU(t *testing.T) {
+	// Interface with MTU=0 would trigger errNoPositiveMTUFound if it could join multicast.
+	// Since fake interfaces can't join multicast groups, this tests the "no usable interfaces" path.
+	ifaces := []net.Interface{
+		{Index: 999, Name: "fake0", Flags: net.FlagUp | net.FlagMulticast, MTU: 0},
+	}
+
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	_, err := NewServer(ipv4.NewPacketConn(sock), nil,
+		WithInterfaces(ifaces...),
+	)
+	// Will fail - either no usable interfaces or MTU error
+	assert.Error(t, err)
+}
+
+func TestNewServerIncludeLoopbackDualStack(t *testing.T) {
+	// Test IncludeLoopback with both IPv4 and IPv6 to cover SetMulticastLoopback paths.
+	// This covers lines 322, 330, 340, 349 in server.go.
+	sock4 := createListener4(t)
+	sock6 := createListener6(t)
+
+	server, err := NewServer(
+		ipv4.NewPacketConn(sock4),
+		ipv6.NewPacketConn(sock6),
+		WithIncludeLoopback(true),
+	)
+	if err != nil {
+		// May fail on some systems where loopback can't join multicast
+		t.Skipf("could not create dual-stack server with IncludeLoopback: %v", err)
+	}
+
+	assert.NoError(t, server.Close())
+}
+
+func TestNewServerIncludeLoopbackIPv4Only(t *testing.T) {
+	// Test IncludeLoopback with IPv4 only
+	sock4 := createListener4(t)
+
+	server, err := NewServer(
+		ipv4.NewPacketConn(sock4),
+		nil,
+		WithIncludeLoopback(true),
+	)
+	if err != nil {
+		t.Skipf("could not create IPv4 server with IncludeLoopback: %v", err)
+	}
+
+	assert.NoError(t, server.Close())
+}
+
+func TestNewServerIncludeLoopbackIPv6Only(t *testing.T) {
+	// Test IncludeLoopback with IPv6 only
+	sock6 := createListener6(t)
+
+	server, err := NewServer(
+		nil,
+		ipv6.NewPacketConn(sock6),
+		WithIncludeLoopback(true),
+	)
+	if err != nil {
+		t.Skipf("could not create IPv6 server with IncludeLoopback: %v", err)
+	}
+
+	assert.NoError(t, server.Close())
+}
+
+func TestConfigurePacketConn4Nil(t *testing.T) {
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	result := configurePacketConn4(nil, "test", "multicast", log)
+	assert.Nil(t, result)
+}
+
+func TestConfigurePacketConn6Nil(t *testing.T) {
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	result := configurePacketConn6(nil, "test", "multicast", log)
+	assert.Nil(t, result)
+}
+
+func TestEnableLoopback4Nil(t *testing.T) {
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	// Should not panic when called with nil
+	enableLoopback4(nil, "test", "multicast", log)
+}
+
+func TestEnableLoopback6Nil(t *testing.T) {
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	// Should not panic when called with nil
+	enableLoopback6(nil, "test", "multicast", log)
+}
+
+func TestConfigurePacketConn4WithConn(t *testing.T) {
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	pc := ipv4.NewPacketConn(sock)
+	result := configurePacketConn4(pc, "test", "multicast", log)
+	assert.NotNil(t, result)
+}
+
+func TestConfigurePacketConn6WithConn(t *testing.T) {
+	sock := createListener6(t)
+	defer func() { _ = sock.Close() }()
+
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	pc := ipv6.NewPacketConn(sock)
+	result := configurePacketConn6(pc, "test", "multicast", log)
+	assert.NotNil(t, result)
+}
+
+func TestEnableLoopback4WithConn(t *testing.T) {
+	sock := createListener4(t)
+	defer func() { _ = sock.Close() }()
+
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	pc := ipv4.NewPacketConn(sock)
+	// Should not panic
+	enableLoopback4(pc, "test", "multicast", log)
+}
+
+func TestEnableLoopback6WithConn(t *testing.T) {
+	sock := createListener6(t)
+	defer func() { _ = sock.Close() }()
+
+	log := logging.NewDefaultLoggerFactory().NewLogger("mdns")
+	pc := ipv6.NewPacketConn(sock)
+	// Should not panic
+	enableLoopback6(pc, "test", "multicast", log)
+}
