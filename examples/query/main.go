87d33ea751aa5ff40758ba24361ad27d978c523c
Pion <59523206+pionbot@users.noreply.github.com>
2026-01-22 08:43:30 +0000

    Update CI configs to v0.11.37
    Update lint scripts and CI configs.

--- examples/query/main.go
+++ examples/query/main.go
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2023 The Pion community <https://pion.ly>
+// SPDX-FileCopyrightText: 2026 The Pion community <https://pion.ly>
 // SPDX-License-Identifier: MIT
 
 // This example program showcases the use of the mDNS client by querying a previously published address
